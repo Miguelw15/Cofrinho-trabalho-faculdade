f43a51a91d6a2377c8da8cc084434cadcc9a89e3
Miguel <miguelkunzer567@gmail.com>
2025-12-08 23:35:55 -0300

    Adicionado equals e Hashcode para remover objeto

--- cofrinho/Moeda.java
+++ cofrinho/Moeda.java
@@ -1,5 +1,7 @@
 package cofrinho;
 
+import java.util.Objects;
+
 public abstract class Moeda {
     protected double valor;
 
@@ -7,7 +9,24 @@ public abstract class Moeda {
         this.valor = valor;
     }
 
-    public abstract void info();   
+    @Override
+	public int hashCode() {
+		return Objects.hash(valor);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Moeda other = (Moeda) obj;
+		return Double.doubleToLongBits(valor) == Double.doubleToLongBits(other.valor);
+	}
+
+	public abstract void info();   
 
     public abstract double converter();
 }
